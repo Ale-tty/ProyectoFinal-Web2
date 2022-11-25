c01d1c9dd1724c5f11b546ff75003163fb7a7607
Ale-tty <alejandra.p.sanchez.s@gmail.com>
2022-11-25 10:46:56 -0600

    Proyecto Final terminado

---
+++ tarea_6.js
@@ -0,0 +1,23 @@
+//funcion xd
+function despedir(){
+	alert("adios");
+}
+
+function toser(){
+	alert("tienes covid? :0");
+}
+
+function estornudar(){
+	alert("salud");
+}
+
+console.log("holi desde consola :3");
+
+console.log("este es un texto :D");
+
+console.log(20);
+console.log(2.6);
+console.log(1,63);
+console.log(1+2);
+console.log(true);
+console.log(false);
